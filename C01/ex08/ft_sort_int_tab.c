a373d555db01a6b82330a3f53167eff4b8369eb1
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-05-23 20:27:05 +0700

    c01

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,5 +1,31 @@
 
+void selectionSort(int arr[], int n){
+    int i;
+    int min;
+    int j;
+    int temp;
+    i = 0;
+    while(i < n - 1)
+    {
+        min = i;
+        j = i + 1;
+        while (j < n){
+            if(arr[j] < arr[min])
+            {
+                min = j;
+            }
+            j++;
+        }
+        if(min != i)
+        {
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        i++;
+    }
+}
 
 void ft_sort_tab(int *tab, int size){
-    
+    selectionSort(tab, size);
 }
