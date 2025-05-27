f349441b424491b595db275c6cc9427aca86667a
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-05-27 19:23:41 +0700

    Update ft_strncpy.c

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -10,19 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int	i;
+    unsigned int	i;
 
 	i = 0;
-	while ((src[i] != '\0') && (i < n))
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		dest[i] = '\0';
-	}
-	return (dest);
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return dest;
 }
