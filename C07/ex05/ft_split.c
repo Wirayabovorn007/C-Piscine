e59f95b1f7e87068ebacd92db007e7329e309199
Wirayabovorn Boonpriam <wiboonpr@z2t10c4.42bangkok.com>
2025-06-07 15:31:42 +0700

    C07/ex05

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -1,87 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiboonpr <wiboonpr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 14:12:49 by wiboonpr          #+#    #+#             */
+/*   Updated: 2025/06/07 15:31:25 by wiboonpr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 int	len(char *c)
 {
 	int	i;
-	
+
 	i = 0;
-	while(c[i])
+	while (c[i])
 		i++;
 	return (i);
 }
 
-int	check(char *str, char *ch, int i)
+int	check(char *s, char *c, int i)
 {
-	int d;
+	int	j;
 
-	d = 0;
-	while (ch[d])
-		if (str[i++] != ch[d++])
+	j = 0;
+	while (c[j])
+	{
+		if (s[i + j] != c[j])
 			return (0);
+		j++;
+	}
 	return (1);
 }
 
 int	count_word(char *str, char *ch)
 {
-	int c;
-	int	i;
+	int		i;
+	int		count;
+	char	lastc;
 
-	c = 0;
 	i = 0;
-	while (str[i])
+	count = 0;
+	if (!*str || !*ch)
+		return (0);
+	if (len(str) > 0)
+		lastc = str[0];
+	while (i < len(str))
 	{
-		if (check(str, ch, i))
+		if ((check(str, ch, i) || str[i] == '\0') && lastc != )
 		{
-			c += 1;
+			count++;
 			i += len(ch);
-			continue ;
 		}
-		i++;
+		else
+			i++;
+		lastc = str[i];
 	}
-	return (c);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (count);
 }
 
 char	**ft_split(char *str, char *charset)
 {
 	int		c;
 	int		i;
-	int		j;
-	int		l;
 	char	*temp;
 	char	**result;
 
 	i = 0;
-	l = 0;
 	c = count_word(str, charset);
-	result = malloc(sizeof(char *) * (c + 1));
+	result = malloc(sizeof(int *) * (c + 1));
 	if (!result)
 		return (NULL);
-	temp = malloc(sizeof(char) * (c + 1));
-	while (i < c - 1)
+	result[c + 1] = '\0';
+	while (i < c)
 	{
-		j = i;
-		while (!check(str, charset, j++))
-		{
-			temp = realloc(temp, len(str[j]));
-			temp[l++] = str[j];
-		}
-		result[i] = malloc(sizeof(char) * len(temp));
-		ft_strcpy(result[i], temp);
-		i++;
+		
 	}
-	return (result);
 }
