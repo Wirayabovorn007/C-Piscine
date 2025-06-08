9fa9cd824cdafc2ebf22483a4a8e41744f8db1b5
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-06-08 20:34:13 +0700

    C08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,17 @@
+#ifndef FT_BOOLEAN_H
+#define FT_BOOLEAN_H
+
+#include <unistd.h>
+
+typedef enum
+{
+	FALSE = 0,
+	TRUE = 1
+} t_bool;
+
+#define EVEN(nbr) ((nbr) % 2 == 0)
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG "I have an odd number of arguments."
+#define SUCCESS 0
+
+#endif
---
+++ C08/ex03/ft_point.h
@@ -0,0 +1,11 @@
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct ft_point
+{
+	int	x;
+	int	y;
+} t_point;
+
+
+#endif
