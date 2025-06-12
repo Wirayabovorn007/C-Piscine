eedc44183477ba9842c684f78ba7f588b14a724b
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-06-12 21:33:37 +0700

    ft_split

---
+++ exam/ft_split/ft_split.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+
+int is_sep(char c, char *sep)
+{
+	while (*sep)
+	{
+		if (c == *sep)
+			return (1);
+		sep++;
+	}
+	return (0);
+}
+
+int count_words(char *str, char *sep)
+{
+	int i =0;
+	int in_word = 0;
+
+	while (*str)
+	{
+		if (is_sep(*str, sep))
+			in_word = 0;
+		else
+		{
+			if (!in_word)
+			{
+				in_word = 1;
+				i++;
+			}
+		}
+		str++;
+	}
+	return (i);
+}
+
+char *word_dup(char *str, int len)
+{
+	int i =0;
+	char *dup = malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	while (i < len)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+
+char	**ft_split(char *str, char *sep)
+{
+	int i = 0;
+	int len;
+	int words = count_words(str, sep);
+
+	char **res = malloc(sizeof(char *) * (words + 1));
+	if (!res)
+		return (NULL);
+	while (i < words)
+	{
+		if (is_sep(*str, sep))
+			str++;
+		else
+		{
+			char *start = str;
+			len = 0;
+			while (*str && !is_sep(*str, sep))
+			{
+				str++;
+				len++;
+			}
+			res[i++] = word_dup(start, len);
+		}
+	}
+	res[i] = NULL;
+	return (res);
+}
+
+// #include <stdio.h>
+// int	main()
+// {
+// 	char str[] = "Banana";
+// 	char *sep = "a";
+// 	char **res = ft_split(str, sep);
+// 	int i =0;
+// 	while (res[i])
+// 	{
+// 		printf("[%d]: %s\n", i,res[i]);
+// 		i++;
+// 	}
+// }
