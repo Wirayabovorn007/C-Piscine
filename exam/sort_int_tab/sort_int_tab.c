225ed60700ffc9aaea12adbdcf3111a3d0c7f979
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-06-12 19:37:56 +0700

    selection sort

--- exam/sort_int_tab/sort_int_tab.c
+++ exam/sort_int_tab/sort_int_tab.c
@@ -4,12 +4,12 @@ void	sort_int_tab(int *tab, unsigned int size)
 {
 	int i =0;
 	int j;
-	int min_ind;
 	int temp;
+	int min_ind;
 	while (i < size)
 	{
 		min_ind = i;
-		j = i+1;
+		j = i + 1;
 		while (j < size)
 		{
 			if (tab[j] < tab[min_ind])
@@ -17,7 +17,7 @@ void	sort_int_tab(int *tab, unsigned int size)
 			j++;
 		}
 		temp = tab[i];
-		tab[i] = tab[min_ind];
+		tab[i]= tab[min_ind];
 		tab[min_ind] = temp;
 		i++;
 	}
