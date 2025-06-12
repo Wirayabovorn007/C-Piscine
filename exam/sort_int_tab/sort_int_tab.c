20b86ad4eea429b5cdb4cca5808a4b08f3e023bf
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-06-12 19:34:45 +0700

    sort int tab

---
+++ exam/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,34 @@
+
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	int i =0;
+	int j;
+	int min_ind;
+	int temp;
+	while (i < size)
+	{
+		min_ind = i;
+		j = i+1;
+		while (j < size)
+		{
+			if (tab[j] < tab[min_ind])
+				min_ind = j;
+			j++;
+		}
+		temp = tab[i];
+		tab[i] = tab[min_ind];
+		tab[min_ind] = temp;
+		i++;
+	}
+}
+
+// #include <stdio.h>
+// int	main()
+// {
+// 	int arr[] = {3, 4, 5, 99, 2, 0, -9};
+// 	sort_int_tab(arr, 7);
+// 	int i = -1;
+// 	while (++i < 7)
+// 		printf("[%d]: %d\n", i, arr[i]);
+// }
